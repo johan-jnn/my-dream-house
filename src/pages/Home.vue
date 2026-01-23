3aa0c4fb8d21d303ea0c01469f85536c965b72a1
3nzo <151024396+EnzoDiGiovanni@users.noreply.github.com>
2026-01-23 15:38:04 +0100

    create hero section (#19)
    Signed-off-by: Enzo <digiovannienzo1@gmail.com>

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import HeroSection from '@/components/HeroSection.vue'
+</script>
+
 <template>
-  <h1>Welcome on the homepage !</h1>
+  <HeroSection
+    title="My Dream House"
+    subtitle="Bienvenue dans la maison de vos rêves"
+    background-image="/images/house.jpg"
+  />
 </template>

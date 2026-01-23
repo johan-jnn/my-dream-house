cce0b148953537531af46db8fc3b142980bf72be
Johan JANIN <johan.janin@gmail.com>
2026-01-23 15:02:53 +0100

    feat: Add & setup vite router (#16)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
 import "./app.css";
-import App from './App.vue';
+import App from "./App.vue";
+import routes from "./routes";
 
-createApp(App).mount('#app')
+createApp(App).use(routes).mount("#app");
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import { createMemoryHistory, createRouter } from "vue-router";
+
+export default createRouter({
+  history: createMemoryHistory(),
+  routes: [],
+});

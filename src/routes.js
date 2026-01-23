84249127a58e108c0925584aba7c9f8cfa9e7ee8
Johan JANIN <johan.janin@gmail.com>
2026-01-23 15:07:23 +0100

    feat: Associed pages to routes (#16)

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,17 @@
 import { createMemoryHistory, createRouter } from "vue-router";
+import Contact from "./pages/Contact.vue";
+import Home from "./pages/Home.vue";
 
 export default createRouter({
   history: createMemoryHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/contact",
+      component: Contact,
+    },
+  ],
 });

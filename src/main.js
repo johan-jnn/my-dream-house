e36794aca947be49c357aea330ee3254d77a23c9
johan-jnn <johan-jnn@users.noreply.github.com>
2026-02-09 13:46:50 +0000

    [MERGE-RELEASE] Fix release 1.0.0

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "./app.css";
-import App from "./App.vue";
-import routes from "./routes";
-
-createApp(App).use(routes).mount("#app");
--- src/pages/Contact.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Welcome on the contact page !</h1>
-</template>
